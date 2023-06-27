4b52897f9a366646b0baf260b5dd72a9cb859546
Kent <dylankentish@live.co.uk>
2023-06-27 23:06:42 +0100

    simplified authOptions
    removed unused dependencies

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
-import { getAuthOptions } from '@/server/auth/options';
+import { authOptions } from '@/server/auth/options';
 import NextAuth from 'next-auth';
 
-const handler = NextAuth(getAuthOptions());
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- src/server/auth/options.ts
+++ src/server/auth/options.ts
@@ -3,94 +3,75 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import { getCsrfToken } from 'next-auth/react';
 import { SiweMessage } from 'siwe';
 import { env } from '@/env.mjs';
-import type { NextApiRequest } from 'next';
 import { prisma } from '../db';
-import { cookies, headers } from 'next/headers';
-import { getToken } from 'next-auth/jwt';
 
-function getAuthOptions() {
-  const options: NextAuthOptions = {
-    session: {
-      strategy: 'jwt',
-    },
-    providers: [
-      CredentialsProvider({
-        name: 'Ethereum',
-        credentials: {
-          message: {
-            label: 'Message',
-            type: 'text',
-            placeholder: '0x0',
-          },
-          signature: {
-            label: 'Signature',
-            type: 'text',
-            placeholder: '0x0',
-          },
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Ethereum',
+      credentials: {
+        message: {
+          label: 'Message',
+          type: 'text',
+          placeholder: '0x0',
         },
-        async authorize(credentials, req) {
-          try {
-            const siwe = new SiweMessage(JSON.parse(credentials?.message || '{}'));
+        signature: {
+          label: 'Signature',
+          type: 'text',
+          placeholder: '0x0',
+        },
+      },
+      async authorize(credentials, req) {
+        try {
+          const siwe = new SiweMessage(JSON.parse(credentials?.message || '{}'));
 
-            const nonce = await getCsrfToken({ req: { ...req, body: undefined } });
+          const nonce = await getCsrfToken({ req: { ...req, body: undefined } });
 
-            const result = await siwe.verify({
-              signature: credentials?.signature || '',
-              domain: env.VERIFIED_DOMAIN,
-              nonce,
-            });
+          const result = await siwe.verify({
+            signature: credentials?.signature || '',
+            domain: env.VERIFIED_DOMAIN,
+            nonce,
+          });
 
-            if (result.success) {
-              return {
-                id: siwe.address,
-                role: 'USER',
-              };
-            } else {
-              return null;
-            }
-          } catch (e) {
-            console.error(e);
+          if (result.success) {
+            return {
+              id: siwe.address,
+              role: 'USER',
+            };
+          } else {
             return null;
           }
-        },
-      }),
-    ],
-    callbacks: {
-      async session({ session, token }) {
-        const dbUser = await prisma.user.upsert({
-          where: {
-            id: token.id,
-          },
-          update: {},
-          create: {
-            id: token.id,
-          },
-        });
-
-        session.user = dbUser;
-
-        return session;
-      },
-      jwt({ token, user }) {
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        if (user) {
-          token.id = user.id;
+        } catch (e) {
+          console.error(e);
+          return null;
         }
-
-        return token;
       },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      const dbUser = await prisma.user.upsert({
+        where: {
+          id: token.id,
+        },
+        update: {},
+        create: {
+          id: token.id,
+        },
+      });
+      session.user = dbUser;
+      return session;
     },
-  };
+    jwt({ token, user }) {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (user) {
+        token.id = user.id;
+      }
 
-  return options;
-}
-
-const getServerActionToken = async () => {
-  const req = { cookies: cookies(), headers: headers() } as unknown as NextApiRequest;
-
-  const token = await getToken({ req });
-
-  return token;
+      return token;
+    },
+  },
 };
-
-export { getAuthOptions, getServerActionToken };
